5473f7b147e4a14551ec366f6df823f183cf6b24
lychkoalexander <lychkoalexander@gmail.com>
2025-04-22 20:30:16 +0300

    mvp

--- discord_bot.py
+++ discord_bot.py
@@ -1,19 +1,19 @@
-import discord
+from contextlib import nullcontext
+
 import asyncpg
+import discord
 from discord.ext import commands
 from discord.ui import View, Button
-
 TOKEN = 'MTM2Mzk0NDQxOTcwNjkyOTQyNQ.GxTMM3.h05THtkpNeL8s4Idn4O76dJcFTRYFJneSo30qA'
+
 PREFIX = '!'
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-# Declare db_pool as None initially
 db_pool = None
 
-# Ensure db_pool is created asynchronously when bot is ready
+# Declare db_pool as None initially
 @bot.event
 async def on_ready():
     global db_pool
@@ -26,7 +26,7 @@ async def on_ready():
     )
     print(f'Bot connected as {bot.user}')
 
-# Команда info
+# Команда help
 @bot.command()
 async def info(ctx):
     info_text = (
@@ -39,8 +39,7 @@ async def info(ctx):
     )
     await ctx.send(info_text)
 
-
-# Команда help
+# Команда info
 @bot.command()
 async def help_command(ctx):
     help_text = (
@@ -50,35 +49,6 @@ async def help_command(ctx):
         "`!choose_game` - Обрати гру для гри"
     )
     await ctx.send(help_text)
-
-
-class RulesOrLinkView(View):
-    def __init__(self, game_id):
-        super().__init__(timeout=None)
-        self.game_id = game_id
-
-    @discord.ui.button(label="📖 Переглянути правила", style=discord.ButtonStyle.secondary)
-    async def view_rules(self, interaction: discord.Interaction, button: Button):
-
-        await interaction.response.send_message(f"Правила гри з ID `{self.game_id}` тут!", ephemeral=True)
-
-class GameButton(Button):
-    def __init__(self, label, game_id):
-        super().__init__(label=label, style=discord.ButtonStyle.primary, custom_id=str(game_id))
-        self.game_id = game_id
-
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_message(
-            f"Ви обрали **{self.label}**. Що бажаєте зробити?",
-            view=RulesOrLinkView(self.game_id),
-            ephemeral=True
-        )
-
-class GameSelectionView(View):
-    def __init__(self, games):
-        super().__init__(timeout=None)
-        for row in games:
-            self.add_item(GameButton(label=row['name'], game_id=row['id']))
 
 @bot.command()
 async def choose_game(ctx):
@@ -90,6 +60,72 @@ async def choose_game(ctx):
 
         view = GameSelectionView(rows)
         await ctx.send("🎲 Обери гру:", view=view)
+
+class GameSelectionView(View):
+    def __init__(self, games):
+        super().__init__(timeout=None)
+        for row in games:
+            self.add_item(GameButton(label=row['name'], game_id=row['id']))
+
+class GameButton(Button):
+    def __init__(self, label, game_id):
+        super().__init__(label=label, style=discord.ButtonStyle.primary, custom_id=str(game_id))
+        self.game_id = game_id
+        self.rows = []
+
+    async def select_rules(self):
+        async with db_pool.acquire() as conn:
+            self.rows = await conn.fetch(f"SELECT id, name FROM rules_category WHERE game_id = {self.game_id} AND parent_id IS NULL")
+            if not self.rows:
+                return
+            return RulesMenu(self.rows)
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"Ви обрали **{self.label}**. Які правила бажаєте дізнатись?",
+            view = await self.select_rules(),
+            ephemeral=True
+        )
+
+class RulesMenu(View):
+    def __init__(self, rules):
+        super().__init__(timeout=None)
+        for row in rules:
+            self.add_item(RulesButton(label=row['name'], rule_id=row['id']))
+
+
+class RulesButton(Button):
+    def __init__(self, label, rule_id):
+        super().__init__(label=label, style=discord.ButtonStyle.primary, custom_id=str(rule_id))
+        self.rule_id = rule_id
+        self.rows = []
+
+    async def select_rules(self):
+        async with db_pool.acquire() as conn:
+            self.rows = await conn.fetch(f"SELECT id, content FROM rules WHERE category_id = {self.rule_id}")
+            if self.rows:
+                return self.rows[0][1]
+            return
+
+    async def select_category(self):
+        async with db_pool.acquire() as conn:
+            self.rows = await conn.fetch(f"SELECT id, name FROM rules_category WHERE parent_id = {self.rule_id}")
+            if not self.rows:
+                return
+            return RulesMenu(self.rows)
+
+    async def callback(self, interaction: discord.Interaction):
+        if await self.select_rules():
+            await interaction.response.send_message(
+                f"{await self.select_rules()}",
+                view=await self.select_category(),
+                ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                view=await self.select_category(),
+                ephemeral=True
+            )
 
 bot.run(TOKEN)
 
---
+++ queries.sql
@@ -0,0 +1,8 @@
+﻿SELECT id, name
+FROM rules_category
+WHERE game_id = self.game_id
+  AND parent_id IS NULL
+
+SELECT id, content FROM rules WHERE category_id = {self.rule_id}
+
+SELECT id, name FROM rules_category WHERE parent_id = {self.rule_id}
